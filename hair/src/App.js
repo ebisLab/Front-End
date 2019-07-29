6250dfc103ef7b597f4f1cd8204d2cf487851880
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-29 12:35:06 -0600

    Commented out areas of Index.js and App.js that were not being used or are non-existent so that react can compile code from AddStylist.js. Will uncomment when finished building component

--- hair/src/App.js
+++ hair/src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import PrivateRoute from "./components/PrivateRoute";
-import Login from "./components/Login";
-import Stylists from "./components/Stylists";
+// import PrivateRoute from "./components/PrivateRoute";
+// import Login from "./components/Login";
+// import Stylists from "./components/Stylists";
 import AddStylist from "./components/AddStylist";
-import UserSignUp from "./components/UserSignUp";
-import StylistSignUp from "./components/StylistSignUp";
+// import UserSignUp from "./components/UserSignUp";
+// import StylistSignUp from "./components/StylistSignUp";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Login} />
+      <AddStylist/>
+      {/* <Route exact path="/" component={Login} />
       <PrivateRoute exact path="/stylists" component={Stylists} />
       <PrivateRoute exact path="/addstylist" component={AddStylist} />
       <PrivateRoute exact path="/newuser" component={UserSignUp} />
-      <PrivateRoute exact path="/newstylist" component={StylistSignUp} />
+      <PrivateRoute exact path="/newstylist" component={StylistSignUp} /> */}
     </div>
   );
 }
