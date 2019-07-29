972dd933b11ecd346735802434d5bf7d5d01a040
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-29 13:19:47 -0600

    Created form and it's inputs as well as the useState hook

--- hair/src/components/AddStylist.js
+++ hair/src/components/AddStylist.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 export default function AddStylist(props) {
+    const [newStylist, setNewStylist] = useState({name:" ", number:" ", email:" "})
+
     return (
-        <div>
-            This is from AddStylist
-        </div>
+        <form>
+            {/* Name */}
+            <div className="form-group">
+                <label>Name:</label>
+                <input
+                    type="text"
+                    className="input"
+                    name="name"
+                    placeholder="Please enter your name"
+                    // value={newStylist.name}
+                    // onChange={handleChange}
+                />
+            </div>
+            {/* Phone Number */}
+            <div className="form-group">
+                <label>Phone Number:</label>
+                <input
+                    type="text"
+                    className="input"
+                    name="number"
+                    placeholder="Please enter your number"
+                    // value={newStylist.number}
+                    // onChange={handleChange}
+                />
+            </div>
+            {/* Email */}
+            <div className="form-group">
+                <label>Email:</label>
+                <input
+                    type="text"
+                    className="input"
+                    name="email"
+                    placeholder="Please enter your email"
+                    // value={newStylist.email}
+                    // onChange={handleChange}
+                />
+            </div>
+        </form>
     )
 
 };
