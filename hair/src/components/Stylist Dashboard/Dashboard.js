6be474a7c1ad0203e50ef7fbab5453472508ad8d
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-30 13:38:10 -0600

    creating dummy data to make sure functionality works

--- hair/src/components/Stylist Dashboard/Dashboard.js
+++ hair/src/components/Stylist Dashboard/Dashboard.js
@@ -4,16 +4,40 @@ import { Route, Link } from "react-router-dom";
 import AddEditForm from "./AddEditForm";
 import StylistPosts from "./StylistPosts";
 
-export default function Dashboard (props) {
+export default function Dashboard () {
 
-    const [post, setPost] =([]);
+    const [stylistPost, setStylistPost] =([
+        {
+            id:1,
+            image:
+        }
+    ]);
 
+    // //Add posts
+    // const addPost = post => {
+    //     setStylistPost([...stylistPost, {...post, id:Date.now()}
+    //     ])
+    // }
 
+    // //Edit Posts
+
+    // const editPost = editedPost => {
+    //     const postCopy = [...stylistPost];
+    //     const oldStylistPost = postCopy.find(post =>
+    //         post.id === editedPost.id);
+
+    //         Object.assign(oldStylistPost, editedPost )
+    //         setStylistPost(postCopy)
+    // }
 
     return(
         <div>
             This is the Dashboard <br></br>
             Nav and header will go here
+            <StylistPosts 
+             stylistPost={stylistPost}
+             setStylistPost={setStylistPost}
+             />
         </div>
     );
 }
--- hair/src/components/Stylist Dashboard/StylistPosts.js
+++ hair/src/components/Stylist Dashboard/StylistPosts.js
@@ -1,1 +1,19 @@
 import React from "react";
+
+export default function Posts (props) {
+    const{ post, buttonText} = props
+    console.log(props)
+    return (
+        <div>
+            <h1>Posts</h1>
+            {props.stylistPost.map((post, index) => {
+                return (
+                    <div className='card' key={index}>
+                        <p>Name;{post.name}</p>
+                    </div>
+                )
+            })}
+            {/* { console.log(props) } */}
+        </div>
+    )
+}
