e9abb96d625284954708f664896af7778eaf0979
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-30 00:25:28 -0600

    Added very minimal styling to center items. More will be needed.

--- hair/src/App.js
+++ hair/src/App.js
@@ -17,20 +17,7 @@ import StylistsMember from "./components/Stylists";
 
 function App() {
   //sets the stylist to
-  const [stylist, setStylist] = useState( 
-    [{
-      id:1, 
-      name:'Jade',
-      number: '456-123-3698',
-      email:'jade@gmail.com'
-   },
-   {
-      id:2, 
-      name:'Alexis',
-      number: '888-369-8465',
-      email:'Alexis@gmail.com'
-   }]
-  );
+  const [stylist, setStylist] = useState([]);
 
   //Adds stylist to form on submission
   const addStylist = person => {
@@ -52,9 +39,11 @@ function App() {
   return (
     <div className="App">
       {/* Change below link to path to login page as home */}
-      <Link to="/">Login</Link> 
-      <Link to="/components/StylistSignUp">Register</Link>
-      <Link to="/components/Stylists">Dashboard</Link>
+      <div className="nav-container">
+          <Link className="navLinks" to="/">Login</Link> 
+          <Link className="navLinks" to="/components/StylistSignUp">Register</Link>
+          <Link className="navLinks" to="/components/Stylists">Dashboard</Link>
+      </div>
 
       <Route path="/components/StylistSignUp" 
              render={props => <StylistSignUp {...props}
